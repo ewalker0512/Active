c41c77a1b919ff538d2bb4eac362cc4d318cd1ce
ewalker0512 <60410770+ewalker0512@users.noreply.github.com>
2020-02-05 15:01:09 -0600

    Add files via upload

---
+++ School.java
@@ -0,0 +1,28 @@
+
+
+
+
+public String runLength(String){
+		 
+		int num = 0
+		
+		for(int i = 0; i < str.length()-1; ++i){
+			
+			for(int a = 0; < str.length()-1; ++a)
+				
+				if(str.toString(str.charAt(i)) == str.toString(str.charAt(a)));
+					num++;
+					if(str.toString(str.charAt(i)) != str.toString(str.charAt(a)));
+					break;
+					return num + str.toString(str.charAt(i));
+		}
+}
+
+public String runLengthChecker( String1, String2) {
+
+
+
+
+
+
+}
